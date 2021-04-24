e3bc31fddb8440b0c92d64b1ef6bfa6ebbabf5c5
RX 570 <m79h67@outlook.com>
2021-04-24 17:23:12 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,28 @@ import * as ContactsAPI from './utils/ContactsAPI'
 import CreateContact from './CreateContact'
 import { Route } from 'react-router-dom'
 
+//contacts is a temporary array 
+const contacts = [
+  {
+    "id": "karen",
+    "name": "Karen Isgrigg",
+    "handle": "karen_isgrigg",
+    "avatarURL": "http://localhost:5001/karen.jpg"
+  },
+  {
+    "id": "richard",
+    "name": "Richard Kalehoff",
+    "handle": "richardkalehoff",
+    "avatarURL": "http://localhost:5001/richard.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "handle": "tylermcginnis",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+ ];
+
 class App extends Component {
   state = {
     contacts: []
@@ -36,6 +58,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <ListContacts contacts ={contacts} />
         <Route exact path='/' render={() => (
           <ListContacts
             contacts={this.state.contacts}
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -77,4 +77,5 @@ class ListContacts extends Component {
   }
 }
 
+
 export default ListContacts
